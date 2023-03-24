aa29d41e0983e0ca6e3de673261d3ca32998c015
dannyp6611 <dannyjp6611@gmail.com>
2023-03-24 12:50:24 +0000

    added city api call

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,1 +1,15 @@
 const KEY = 'ODd7w819augbgLxVRUK42iw9Tu2FNIga';
+
+const getCity = async (city) => {
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${KEY}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+getCity('manchester')
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error));
