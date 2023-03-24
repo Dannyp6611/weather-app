fb482b4a864897dba0efd7a7a2394b85f4751d85
dannyp6611 <dannyjp6611@gmail.com>
2023-03-24 15:30:54 +0000

    update weather information UI

---
+++ scripts/app.js
@@ -0,0 +1,44 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = ({ cityDets, weather }) => {
+  // const cityDets = data.cityDets;
+  // const weather = data.weather;
+
+  // update details template
+  details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>
+  `;
+
+  // remove the display none from card if it is present
+  if (card.classList.contains('d-none')) {
+    card.classList.remove('d-none');
+  }
+};
+
+const updateCity = async (city) => {
+  const cityDets = await getCity(city);
+  const weather = await getWeather(cityDets.Key);
+
+  return { cityDets, weather };
+};
+
+cityForm.addEventListener('submit', (e) => {
+  // prevent default action
+  e.preventDefault();
+
+  // get city value
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+
+  // update the ui with the new city
+  updateCity(city)
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
+});
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,5 +1,17 @@
 const KEY = 'ODd7w819augbgLxVRUK42iw9Tu2FNIga';
 
+// get weather information
+const getWeather = async (id) => {
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${KEY}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+// get city information
 const getCity = async (city) => {
   const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
   const query = `?apikey=${KEY}&q=${city}`;
@@ -9,7 +21,3 @@ const getCity = async (city) => {
 
   return data[0];
 };
-
-getCity('manchester')
-  .then((data) => console.log(data))
-  .catch((error) => console.log(error));
